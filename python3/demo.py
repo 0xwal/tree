761580a0375d145ce5a7306da800176490f6cedf
BISOON <20063083+BISOON@users.noreply.github.com>
2020-11-19 07:40:38 +0300

    python - dynamic input for demo

--- python3/demo.py
+++ python3/demo.py
@@ -1,12 +1,17 @@
+import sys
+
 from binary_tree import BinaryTree
 
 
 def demo():
     binary = BinaryTree()
-    for i in range(10):
+    for i in range(count):
         binary.add(i)
     binary.dump()
 
 
 if __name__ == '__main__':
+    count = 10
+    if len(sys.argv) > 1 and sys.argv[1].isnumeric():
+        count = int(sys.argv[1])
     demo()
