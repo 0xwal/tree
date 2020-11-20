c25b00c71ee0b537174df83155bd243cebee9ca4
BISOON <20063083+BISOON@users.noreply.github.com>
2020-11-20 16:20:48 +0300

    c - insertion implementation #1

--- c/src/binary-search-tree.c
+++ c/src/binary-search-tree.c
@@ -1,5 +1,8 @@
 #include "include/binary-search-tree.h"
 #include <stdlib.h>
+#include <printf.h>
+
+void bsd_internal_add(node_s* currentNode, int value);
 
 binary_search_tree_s* bst_create()
 {
@@ -13,5 +16,39 @@ void bst_add(binary_search_tree_s* bst, int value)
     {
         bst->root = (node_s*)calloc(1, sizeof(node_s));
         bst->root->data = value;
+        return;
+    }
+
+    bsd_internal_add(bst->root, value);
+}
+
+void bsd_internal_add(node_s* currentNode, int value)
+{
+    while (1)
+    {
+        if (currentNode->data < value)
+        {
+            if (currentNode->right != NULL)
+            {
+                currentNode = currentNode->right;
+                continue;
+            }
+            currentNode->right = calloc(1, sizeof(node_s));
+            currentNode->right->data = value;
+
+            break;
+        }
+
+        if (currentNode->data > value)
+        {
+            if (currentNode->left != NULL)
+            {
+                currentNode = currentNode->left;
+                continue;
+            }
+            currentNode->left = calloc(1, sizeof(node_s));
+            currentNode->left->data = value;
+            break;
+        }
     }
 }
--- c/tests/binary-search-tree.test.cpp
+++ c/tests/binary-search-tree.test.cpp
@@ -2,6 +2,16 @@
 
 #include <catch2/catch.hpp>
 #include <binary-search-tree.h>
+#include <traversal.h>
+
+#define COMPARE_NODES(clistPointer, values...) \
+    int expectedValues[] = values;         \
+    int expectedSize = sizeof(expectedValues) / 4; \
+    REQUIRE(expectedSize == clistPointer->size); \
+    for (int i = 0; i < expectedSize; ++i) \
+    {                                        \
+        REQUIRE(clistPointer->data[i] == expectedValues[i]);\
+    }
 
 TEST_CASE("binary search tree")
 {
@@ -11,27 +21,71 @@ TEST_CASE("binary search tree")
         REQUIRE(bst->root == NULL);
     }
 
-    SECTION("when adding for first time we should assign the node to root")
+    SECTION("addition")
     {
-        binary_search_tree_s* bst = bst_create();
-        bst_add(bst, 4);
-        REQUIRE(bst->root != NULL);
-        REQUIRE(bst->root->data == 4);
-    }
+        SECTION("when adding for first time we should assign the node to root")
+        {
+            binary_search_tree_s* bst = bst_create();
+            bst_add(bst, 4);
+            REQUIRE(bst->root != NULL);
+            REQUIRE(bst->root->data == 4);
+        }
 
-    SECTION("added node should have NULL for both right and left")
-    {
-        binary_search_tree_s* bst = bst_create();
-        bst_add(bst, 4);
-        REQUIRE(bst->root->right == NULL);
-        REQUIRE(bst->root->left == NULL);
-    }
+        SECTION("added node should have NULL for both right and left")
+        {
+            binary_search_tree_s* bst = bst_create();
+            bst_add(bst, 4);
+            REQUIRE(bst->root->right == NULL);
+            REQUIRE(bst->root->left == NULL);
+            bst_add(bst, 6);
+            REQUIRE(bst->root->right->right == NULL);
+            REQUIRE(bst->root->right->left == NULL);
+        }
 
-    SECTION("when adding for second or more time we should not change the root")
-    {
-        binary_search_tree_s* bst = bst_create();
-        bst_add(bst, 4);
-        bst_add(bst, 5);
-        REQUIRE(bst->root->data == 4);
+        SECTION("when adding for second or more time we should not change the root node")
+        {
+            binary_search_tree_s* bst = bst_create();
+            bst_add(bst, 4);
+            bst_add(bst, 5);
+            REQUIRE(bst->root->data == 4);
+            REQUIRE(bst->root->right != nullptr);
+            REQUIRE(bst->root->right->data == 5);
+        }
+
+
+        SECTION("should add a larger value of the current node to right")
+        {
+            binary_search_tree_s* bst = bst_create();
+            bst_add(bst, 4);
+            bst_add(bst, 5);
+            bst_add(bst, 6);
+            clist_s* list = clist_init(16);
+            preorder_traversal(bst->root, list);
+            COMPARE_NODES(list, { 4, 5, 6 });
+        }
+
+        SECTION("should add a smaller value than the current node to left")
+        {
+            binary_search_tree_s* bst = bst_create();
+            bst_add(bst, 6);
+            bst_add(bst, 5);
+            bst_add(bst, 4);
+            clist_s* list = clist_init(16);
+            preorder_traversal(bst->root, list);
+            COMPARE_NODES(list, { 6, 5, 4 });
+        }
+
+        SECTION("mixed with larger and smaller")
+        {
+            binary_search_tree_s* bst = bst_create();
+            bst_add(bst, 6);
+            bst_add(bst, 8);
+            bst_add(bst, 5);
+            bst_add(bst, 3);
+            bst_add(bst, 4);
+            clist_s* list = clist_init(16);
+            preorder_traversal(bst->root, list);
+            COMPARE_NODES(list, { 6, 5, 3, 4, 8 });
+        }
     }
 }
